94b7291834b534a1a08f751572602a85a2e6ba39
Elfiex <vadim192000@gmail.com>
2022-09-12 18:28:01 +0300

    реализовано смена темы

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,39 +1,66 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
 import Select from 'react-select';
 import { ChangeTheme } from '../../assets/icons/global/ChangeTheme';
 import { GlobalSvgSelector } from '../../assets/icons/global/GlobalSvgSelector';
 
 import s from './Header.module.scss';
 
-const options = [
-    { value: 'city-1', label: 'Москва' },
-    { value: 'city-2', label: 'Красногорск' },
-];
-
-const colourStyles = {
-    control: styles => ({
-        ...styles,
-        backgroundColor: 'rgba(71, 147, 255, 0.2)',
-        width: '194px', 
-        height: '37px',
-        border: 'none',
-        borderRadius: '10px',
-        zIndex: 100
-    })
-}
-
 export const Header = () => {
+    const options = [
+        { value: 'city-1', label: 'Москва' },
+        { value: 'city-2', label: 'Красногорск' },
+    ];
     
+    const [theme, setTheme] = useState('light');
+
+    const colourStyles = {
+        control: (styles) => ({
+            ...styles,
+            backgroundColor: theme === 'dark' ? '#4f4f4f' : 'rgba(71, 147, 255, 0.2)',
+            width: '194px',
+            height: '37px',
+            border: 'none',
+            borderRadius: '10px',
+            zIndex: 100,
+        }),
+        singleValue: (styles) => ({
+            ...styles,
+            color: theme === 'dark' ? '#fff' : '#000',
+        }),
+    };
+    
+    function changeTheme() {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    }
+
+    useEffect(() => {
+        const root = document.querySelector(':root');
+
+        const components = [
+            'body-background',
+            'components-background',
+            'card-background',
+            'card-shadow',
+            'text-color',
+        ];
+
+        components.forEach(component => {
+            root.style.setProperty(
+                `--${component}-default`, 
+                `var(--${component}-${theme})`
+            );
+        });
+    }, [theme]);
 
     return  (
         <header className={s.header}>
             
         <div className={s.wrapper}>
-                <div className={s.logo}><GlobalSvgSelector/></div>
+                <div className={s.logo}><GlobalSvgSelector id="header-logo"/></div>
                 <div className={s.title}>React weather</div>
             </div>
             <div className={s.wrapper}>
-                <div className={s.change_theme}>
+                <div className={s.change_theme} onClick={changeTheme}>
                     <ChangeTheme id='change-theme'/>
                 </div>
                 <Select 
